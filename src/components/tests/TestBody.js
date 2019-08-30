e4ffbe90e6f0bb07280e9ddd76e776dc6f4c2867
Daniil <mytasmic@gmail.com>
2019-08-31 02:07:55 +0300

    barely working test player

--- src/components/tests/TestBody.js
+++ src/components/tests/TestBody.js
@@ -5,28 +5,42 @@ export default function TestBody() {
     const testData = {
         items: [
             {
-                content: "Foo",
+                content: "Кто я - тварь дрожащая или право имею?",
                 id: 1,
-                response: null
+                response: null,
+                scale: {
+                    min: 0,
+                    max: 4,
+                    minLabel: "Тварь дрожащая",
+                    maxLabel: "Право имею"
+                }
             },
             {
-                content: "Bar",
+                content: "Я рыбы отведал и пали покровы, я видел сквозь марево дня как движется по небу витязь багровый, чье око взыскует меня.",
                 id: 2,
-                response: null
+                response: null,
+                scale: {
+                    min: 0,
+                    max: 4,
+                    minLabel: "Не согласен",
+                    maxLabel: "Согласен"
+                }
             }
         ],
         id: 1,
         type: "likert"
     }
-
-    const handleResponse = (item) => {
-        setTest(
-            ...test,
-            //test.find(i => i.id === item.id);
-            // https://stackoverflow.com/questions/39889009/replace-object-in-array-on-react-state
-        );
-    }
     const [ test, setTest ] = useState(testData);
+
+    const handleResponse = (itemId, response) => {
+        setTest({
+            ...test,
+            items: test.items.map(item => {
+                return item.id === itemId ? {...item, response: response} : {...item}
+            })
+        });
+    }
+    
     return test.items.length ? (
         <div>
             { test.items.map(item => {
--- src/components/tests/TestItemLikert.js
+++ src/components/tests/TestItemLikert.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Slider } from '@vkontakte/vkui';
+import './TestItemLikert.css';
 
 export default function TestItemLikert(props) {
     const { item, handleResponse } = props;
     return (
-        <div id={item.id}>
-            { item.content }
+        <div className="itemWrapper" id={item.id}>
+            <h3>{ item.content }</h3>
+            <i className="scaleLabel">&larr; { item.scale.minLabel } / { item.scale.maxLabel } &rarr;</i>
+            <Slider
+                step={1}
+                min={item.scale.min}
+                max={item.scale.max}
+                // value={Number(this.state.value2)}
+                onChange={value => {handleResponse(item.id, value)}}
+            />
+            <b>Response: { item.response }</b>
         </div>
     )
 }
